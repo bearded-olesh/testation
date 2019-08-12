23b0f0c0560dc499a176a267dd559293a255b0d5
bearded-olesh <alexeyzharkov88@gmail.com>
2019-08-12 14:39:16 +0300

    #

--- js/main.js
+++ js/main.js
@@ -1,26 +1,22 @@
 (function() {
 	var roomBtns = document.querySelectorAll('.room__button'),
-		room = document.querySelectorAll('.room');
+		rooms = document.querySelectorAll('.room');
 
 	roomBtns.forEach((elem) => {
 		elem.addEventListener('click', () => {
-			elem.parentElement.parentElement.classList.add('room_ready-to-disable');
+			elem.parentElement.parentElement.parentElement.classList.add('room_ready-to-disable');
 		});
 	});
 
-	room.forEach((elem) => {
+	rooms.forEach((elem) => {
 		elem.addEventListener('click', () => {
 			if (elem.classList.contains('room_disabled')) {
-				elem.classList.add('room_ready-to-active');
+				elem.className = 'room';
 			}
 		});
 		elem.addEventListener('mouseleave', () => {
-			if (elem.classList.contains('room_ready-to-active')) {
-				elem.className = 'room';
-				elem.querySelector('.room__button').innerHTML = 'Забронировать';
-			} else if (elem.classList.contains('room_ready-to-disable')) {
+			if (elem.classList.contains('room_ready-to-disable')) {
 				elem.classList.add('room_disabled');
-				elem.querySelector('.room__button').innerHTML = 'Подробнее';
 			}
 		});
 	});
